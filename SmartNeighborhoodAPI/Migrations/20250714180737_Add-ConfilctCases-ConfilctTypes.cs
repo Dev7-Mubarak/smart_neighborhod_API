8ba9c003cbf125d538359a6fd296c16e9b52d1c9
Dev7-Mubarak <mubark7382@gmail.com>
2025-07-15 23:48:08 -0700

    fix: apply migration in remote db

--- SmartNeighborhoodAPI/Migrations/20250714180737_Add-ConfilctCases-ConfilctTypes.cs
+++ SmartNeighborhoodAPI/Migrations/20250714180737_Add-ConfilctCases-ConfilctTypes.cs
@@ -11,17 +11,6 @@ namespace SmartNeighborhoodAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "PersonComplain");
-
-            migrationBuilder.DropTable(
-                name: "Complains");
-
-            migrationBuilder.DropTable(
-                name: "ConfilctParties");
-
-            migrationBuilder.DropTable(
-                name: "ComplainTypes");
 
             migrationBuilder.CreateTable(
                 name: "ConfilctTypes",
@@ -117,89 +106,6 @@ namespace SmartNeighborhoodAPI.Migrations
             migrationBuilder.DropTable(
                 name: "ConfilctTypes");
 
-            migrationBuilder.CreateTable(
-                name: "ComplainTypes",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ComplainTypes", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "ConfilctParties",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ConfilctParties", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Complains",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    ComplainTypeId = table.Column<int>(type: "int", nullable: false),
-                    IsResolved = table.Column<bool>(type: "bit", nullable: false),
-                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Outcome = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    SessionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Complains", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Complains_ComplainTypes_ComplainTypeId",
-                        column: x => x.ComplainTypeId,
-                        principalTable: "ComplainTypes",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "PersonComplain",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    ComplainId = table.Column<int>(type: "int", nullable: false),
-                    ConfilctPartyId = table.Column<int>(type: "int", nullable: false),
-                    PersonId = table.Column<int>(type: "int", nullable: false),
-                    PesonId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PersonComplain", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_PersonComplain_Complains_ComplainId",
-                        column: x => x.ComplainId,
-                        principalTable: "Complains",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_PersonComplain_ConfilctParties_ConfilctPartyId",
-                        column: x => x.ConfilctPartyId,
-                        principalTable: "ConfilctParties",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_PersonComplain_People_PersonId",
-                        column: x => x.PersonId,
-                        principalTable: "People",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
             migrationBuilder.UpdateData(
                 table: "AspNetUsers",
                 keyColumn: "Id",
@@ -207,25 +113,6 @@ namespace SmartNeighborhoodAPI.Migrations
                 columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
                 values: new object[] { "9c4c3d08-b706-48fe-9cfa-ea69c87a65c7", "AQAAAAIAAYagAAAAEEelOrtut6rMF5dDIfCELHkB7Jz4Uw6FvlIJC9Vq4XDshpVwL0ALLpG/UeikEeXyPQ==", "f8294112-0494-4952-bb2e-d38292c3782f" });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Complains_ComplainTypeId",
-                table: "Complains",
-                column: "ComplainTypeId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_PersonComplain_ComplainId",
-                table: "PersonComplain",
-                column: "ComplainId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_PersonComplain_ConfilctPartyId",
-                table: "PersonComplain",
-                column: "ConfilctPartyId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_PersonComplain_PersonId",
-                table: "PersonComplain",
-                column: "PersonId");
         }
     }
 }
--- SmartNeighborhoodAPI/Program.cs
+++ SmartNeighborhoodAPI/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RemoteConnection")));
 builder.Services.AddLocalization(options =>
 {
     options.ResourcesPath = "Resources";
