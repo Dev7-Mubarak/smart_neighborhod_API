d1f4a8d2a9bd91c1c444480aa4942030e2cf22f5
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-11-11 10:08:03 -0800

    add interface

---
+++ SmartNeighborhoodAPI/Interfaces/IAuthorityService.cs
@@ -0,0 +1,13 @@
+﻿using SmartNeighborhoodAPI.Helpers.DTOs.Auth;
+
+namespace SmartNeighborhoodAPI.Interfaces
+{
+    public interface IAuthorityService
+    {
+        Task<ApiResponse<GetAuthorityDto>> AddAuthorityAsync(AddAuthorityDto dto, CancellationToken ct = default);
+        Task<ApiResponse<PaginatedResult<GetAuthorityDto>>> GetAllAuthoritiesAsync(int page = 1, int pageSize = 10, CancellationToken ct = default);
+        Task<ApiResponse<GetAuthorityDto>> GetAuthorityByIdAsync(int id, CancellationToken ct = default);
+        Task<ApiResponse<string>> UpdateAuthorityAsync(int id, UpdateAuthorityDto dto, CancellationToken ct = default);
+        Task<ApiResponse<string>> DeleteAuthorityAsync(int id, CancellationToken ct = default);
+    }
+}
