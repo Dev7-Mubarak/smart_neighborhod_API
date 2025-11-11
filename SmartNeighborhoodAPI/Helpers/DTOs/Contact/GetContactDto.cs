a49ba49b1f0b5bec07e932b51d62bfc33b19cf4c
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-11-11 10:19:31 -0800

    add interface and the remain dto

---
+++ SmartNeighborhoodAPI/Helpers/DTOs/Contact/GetContactDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartNeighborhoodAPI.Helpers.DTOs.Contact
+{
+    public class GetContactDto
+    {
+        public int Id { get; set; }
+        public int AuthorityId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Job { get; set; }
+        public string Phone { get; set; } = null!;
+    }
+}
---
+++ SmartNeighborhoodAPI/Interfaces/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using SmartNeighborhoodAPI.Helpers.DTOs.Contact;
+
+namespace SmartNeighborhoodAPI.Interfaces
+{
+    public interface IContactService
+    {
+        Task<ApiResponse<GetContactDto>> AddContactAsync(int authorityId, AddContactDto dto, CancellationToken ct = default);
+        Task<ApiResponse<IEnumerable<GetContactDto>>> GetContactsByAuthorityAsync(int authorityId, CancellationToken ct = default);
+        Task<ApiResponse<GetContactDto>> GetContactByIdAsync(int id, CancellationToken ct = default);
+        Task<ApiResponse<string>> UpdateContactAsync(int id, UpdateContactDto dto, CancellationToken ct = default);
+        Task<ApiResponse<string>> DeleteContactAsync(int id, CancellationToken ct = default);
+    }
+}
