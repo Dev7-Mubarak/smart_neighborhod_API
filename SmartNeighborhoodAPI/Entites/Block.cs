43ef6e79ffc6eb92f5a25c3235025b4c43a69797
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-11-15 04:40:51 -0800

    add the self join realtion in the block entity

--- SmartNeighborhoodAPI/Entites/Block.cs
+++ SmartNeighborhoodAPI/Entites/Block.cs
@@ -7,9 +7,17 @@ namespace OurProjectSmartNeiborhood.Entites
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public int? ParentBlockId { get; set; }
+
+        public Block ParentBlock { get; set; }
+
+        public ICollection<Block> ChildBlocks { get; set; } = new List<Block>();
+
         public string ManagerId { get; set; }
         [JsonIgnore]
         public AppUser Manager { get; set; }
+
         [JsonIgnore]
         public ICollection<Family> Families { get; set; } = new List<Family>();
     }
