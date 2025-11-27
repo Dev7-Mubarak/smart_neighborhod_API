de5803fd1c986066b73e9a2b2a50db5f00ba115c
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-11-27 06:19:27 -0800

    Add Application Passport

---
+++ SmartNeighborhoodAPI/Entites/PassportApplication.cs
@@ -0,0 +1,23 @@
+﻿using SmartNeighborhoodAPI.Entites.Enums;
+
+namespace SmartNeighborhoodAPI.Entites
+{
+    public class PassportApplication
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ResidentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string ApplicationNumber { get; set; } = string.Empty;
+
+        public ApplicationStatus Status { get; set; } = ApplicationStatus.Received;
+
+
+        public DateTime? LastUpdate { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
