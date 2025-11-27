0c6f338cd177f6a134f32941ca87b7266d68a867
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-11-27 06:16:58 -0800

    create medicine and MedicineBath

---
+++ SmartNeighborhoodAPI/Entites/MedicineBatch.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartNeighborhoodAPI.Entites
+{
+    public class MedicineBatch
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Medicine))]
+        public int MedicineId { get; set; }
+
+        public virtual Medicine? Medicine { get; set; }
+
+        public int Quantity { get; set; }
+
+
+        public DateTime? ArrivalDate { get; set; }
+
+        public DateTime? ExpireDate { get; set; }
+
+        [MaxLength(250)]
+        public string? Provider { get; set; }
+    }
+}
