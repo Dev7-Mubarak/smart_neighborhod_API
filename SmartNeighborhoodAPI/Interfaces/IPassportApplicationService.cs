a91c47ca6e3dc45b4a9e95a62abb99d2bb7c2ade
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-11-27 07:29:33 -0800

    add interface for the feature

---
+++ SmartNeighborhoodAPI/Interfaces/IPassportApplicationService.cs
@@ -0,0 +1,27 @@
+﻿
+using SmartNeighborhoodAPI.Entites.Enums;
+namespace SmartNeighborhoodAPI.Interfaces
+{
+
+
+    public interface IPassportApplicationService
+    {
+        Task<ApiResponse<GetPassportApplicationDto>> AddAsync(AddPassportApplicationDto dto, CancellationToken ct = default);
+
+        Task<ApiResponse<PaginatedResult<GetAllPassportApplicationDto>>> GetAllAsync(
+            string? applicationNumber = null,
+            ApplicationStatus? status = null,
+            int page = 1,
+            int pageSize = 10,
+            CancellationToken ct = default);
+
+        Task<ApiResponse<GetPassportApplicationDto>> GetByIdAsync(int id, CancellationToken ct = default);
+
+        Task<ApiResponse<DetailsPassportApplicationDto>> DetailsAsync(int id, CancellationToken ct = default);
+
+        Task<ApiResponse<string>> UpdateAsync(int id, UpdatePassportApplicationDto dto, CancellationToken ct = default);
+
+        Task<ApiResponse<string>> DeleteAsync(int id, CancellationToken ct = default);
+    }
+
+}
