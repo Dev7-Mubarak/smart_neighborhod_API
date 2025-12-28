dacce32d611a2cfeb9775dc42b8c1606a2f1b30f
Dev7-Mubarak <mubark7382@gmail.com>
2025-12-28 11:52:56 +0300

    Add ResidentialNeighborhoodManager role migration
    Added EF Core migration to seed "ResidentialNeighborhoodManager" role with a specific GUID. Updated seeded admin user credentials (ConcurrencyStamp, PasswordHash, SecurityStamp). Updated migration designer and ApplicationDbContextModelSnapshot accordingly.

---
+++ SmartNeighborhoodAPI/Migrations/20251228083125_add-Residential-Neighborhood-Manager-Role.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartNeighborhoodAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addResidentialNeighborhoodManagerRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName" },
+                values: new object[] { "44444444-4444-4444-4444-444444444444", "ResidentialNeighborhoodManager", "RESIDENTIALNEIGHBORHOODMANAGER" }
+            );
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d071835c-1011-471f-8733-afb655216d7b", "AQAAAAIAAYagAAAAEA9bHObqqv0kW/T+TUz3XpOGUpZsdSFl4OtYOaK3RkHOXcxypeJQDnbRcwdszxqqYw==", "7dc8a3fe-84c2-4821-abea-fe6f0c721351" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "44444444-4444-4444-4444-444444444444"
+);
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "29e853df-296e-4117-b43a-84a5c4b265a9", "AQAAAAIAAYagAAAAEPAseORnKSJZ9S7QldA07dhf0pPsS62wtOHFruiTIq1eh5/chp8q7JHes7ygoqSNRQ==", "df765d19-ac7d-4479-9ade-09152e9592a7" });
+        }
+    }
+}
