edf0fb002fe23fb5c52aeaf9157da19b0e7faf5e
AMAR_B <AMAR_B@DESKTOP-843Q2GH>
2025-12-27 22:11:29 -0800

    feat: add configuration for the feature

---
+++ SmartNeighborhoodAPI/Configuration/ResidentialNeighborhoodConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using SmartNeighborhoodAPI.Entites;
+
+namespace OurProjectSmartNeiborhood.Configuration
+{
+    public class ResidentialNeighborhoodConfiguration
+        : IEntityTypeConfiguration<ResidentialNeighborhood>
+    {
+        public void Configure(EntityTypeBuilder<ResidentialNeighborhood> builder)
+        {
+            builder.HasKey(n => n.Id);
+
+            builder.Property(n => n.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.HasMany(n => n.ResidentialUnits)
+                .WithOne(u => u.ResidentialNeighborhood)
+                .HasForeignKey(u => u.ResidentialNeighborhoodId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+
+}
