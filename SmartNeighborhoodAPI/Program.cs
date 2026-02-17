bed545bebb2168f4ee93b51ae40380de2b0fb697
ALI <a711879040@gmail.com>
2026-02-17 06:42:15 +0300

    Enable ISyncService in Program.cs for synchronization support

--- SmartNeighborhoodAPI/Program.cs
+++ SmartNeighborhoodAPI/Program.cs
@@ -83,7 +83,7 @@ builder.Services.AddScoped<IResidentialNeighborhoodService, ResidentialNeighborh
 builder.Services.AddScoped<ResidentialUnitService>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
 builder.Services.AddScoped<IIssueService, IssueService>();
-//builder.Services.AddScoped<ISyncService, SyncService>();
+builder.Services.AddScoped<ISyncService, SyncService>();
 
 
 
