a563e167fae375f4eb9940b04ff6171f342ce8c1
ALI <a711879040@gmail.com>
2026-02-17 05:29:31 +0300

    Implement ISyncable interface in Family, Issue, and Person classes with soft delete support

--- SmartNeighborhoodAPI/Entites/Family.cs
+++ SmartNeighborhoodAPI/Entites/Family.cs
@@ -1,9 +1,10 @@
 ﻿using System.Text.Json.Serialization;
 using SmartNeighborhoodAPI.Entites.Enums;
+using SmartNeighborhoodAPI.Interfaces;
 
 namespace OurProjectSmartNeiborhood.Entites
 {
-    public class Family
+    public class Family : ISyncable
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -18,6 +19,12 @@ namespace OurProjectSmartNeiborhood.Entites
 
         public ICollection<ProjectFamily> ProjectFamilies { get; set; }
 
+        // ISyncable implementation :-
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public string? ClientId { get; set; }
     }
 
 
